a732a0cbaa7a20c9c765954bb20fe503a61b5c43
Yassss785 <yasminebouhadji02@gmail.com>
2026-01-12 13:12:38 +0100

    nv project TP2

---
+++ src/main/java/org/centrale/medev/exam_medev/EXAM_MEDEV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.exam_medev;
+
+/**
+ *
+ * @author yasminebouhadji
+ */
+public class EXAM_MEDEV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
